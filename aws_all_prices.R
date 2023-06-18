27d23d3585fffadae9153060dbaf3556a8859a96
marceldietl <ge93coc>
2023-06-18 18:49:26 +0200

    Averaged RI over AZs + updated data

--- aws_all_prices.R
+++ aws_all_prices.R
@@ -19,28 +19,35 @@ util.packages.load <- function() {
 
 aws.data.folder <- "./aws_raw_data"
 
+
+## Load data for region "us-east-1"
+
 aws.OD.prices.load <- function() {
-  aws.OD.prices <<- read.csv(paste(aws.data.folder, "OD_rates_2023-06-12.csv", sep = "/"))
+  aws.OD.prices <<- read.csv(paste(aws.data.folder, "OD_rates_2023-06-18.csv", sep = "/"))
 }
 
 aws.RI.prices.load <- function() {
-  aws.RI.prices <<- read.csv(paste(aws.data.folder, "RI_rates_2023-06-12.csv", sep = "/"))
+  aws.RI.prices.1a <- read.csv(paste(aws.data.folder, "RI_rates_useast1a_2023-06-18.csv", sep = "/"))
+  aws.RI.prices.1b <- read.csv(paste(aws.data.folder, "RI_rates_useast1b_2023-06-18.csv", sep = "/"))
+  aws.RI.prices.1c <- read.csv(paste(aws.data.folder, "RI_rates_useast1c_2023-06-18.csv", sep = "/"))
+  
+  aws.RI.prices <<- rbind(aws.RI.prices.1a, aws.RI.prices.1b, aws.RI.prices.1c)
 }
 
 aws.SP.prices.load <- function() {
-  aws.SP.prices <<- read.csv(paste(aws.data.folder, "SP_rates_2023-06-12.csv", sep = "/"))
+  aws.SP.prices <<- read.csv(paste(aws.data.folder, "SP_rates_2023-06-18.csv", sep = "/"))
 }
 
 aws.Spot.prices.load <- function() {
-  aws.Spot.prices.1a <- read.csv(paste(aws.data.folder, "Spot_rates_useast1a_2023-06-12.csv", sep = "/"))
-  aws.Spot.prices.1b <- read.csv(paste(aws.data.folder, "Spot_rates_useast1b_2023-06-12.csv", sep = "/"))
-  aws.Spot.prices.1c <- read.csv(paste(aws.data.folder, "Spot_rates_useast1c_2023-06-12.csv", sep = "/"))
+  aws.Spot.prices.1a <- read.csv(paste(aws.data.folder, "Spot_rates_useast1a_2023-06-18.csv", sep = "/"))
+  aws.Spot.prices.1b <- read.csv(paste(aws.data.folder, "Spot_rates_useast1b_2023-06-18.csv", sep = "/"))
+  aws.Spot.prices.1c <- read.csv(paste(aws.data.folder, "Spot_rates_useast1c_2023-06-18.csv", sep = "/"))
   
   aws.Spot.prices <<- rbind(aws.Spot.prices.1a, aws.Spot.prices.1b, aws.Spot.prices.1c)
 }
 
 aws.Spot.interruption.freq.load <- function() {
-  aws.Spot.interruption.freq <<- read.csv(paste(aws.data.folder, "Spot_interruption_freq_2023-06-12.csv", sep = "/"))
+  aws.Spot.interruption.freq <<- read.csv(paste(aws.data.folder, "Spot_interruption_freq_2023-06-18.csv", sep = "/"))
 }
 
 
@@ -100,6 +107,9 @@ aws.RI.prices.process <- function() {
   
   # calculate duration from seconds to years
   aws.RI.prices[, 'Duration'] <- ifelse(aws.RI.prices[, 'Duration'] == 31536000, 1, 3)
+  
+  # take mean over each AZ
+  aws.RI.prices <- aggregate(RI.costs.hourly ~ API.Name + OfferingClass + OfferingType + Duration, data = aws.RI.prices, mean)
   
   # New column with full pricing name  
   aws.RI.prices$column.names <- 
@@ -167,6 +177,9 @@ aws.Spot.prices.process <- function() {
   # remove all burstable instances t.*
   aws.Spot.prices <- aws.Spot.prices[!grepl("^t", aws.Spot.prices$API.Name), ]
   
+  # get one additional dataset with the minimum price of an instance
+  aws.Spot.prices.min <<- aggregate(Spot.costs.hourly ~ API.Name, data = aws.Spot.prices, min)
+  
   # get mean for each instance over last three months
   aws.Spot.prices <<- aggregate(Spot.costs.hourly ~ API.Name, data = aws.Spot.prices, mean)
 }
@@ -200,6 +213,12 @@ create.all.prices.dataset <- function() {
   aws.all.prices <<- aws.list %>% reduce(inner_join, by = 'API.Name')
 }
 
+# dataset with minimum Spot prices
+create.all.prices.dataset.min <- function() {
+  aws.list <- list(aws.OD.prices, aws.RI.prices, aws.SP.prices, aws.Spot.prices.min, aws.Spot.interruption.freq)
+  aws.all.prices.min <<- aws.list %>% reduce(inner_join, by = 'API.Name')
+}
+
 
 # ------------------------------------------------------------------------------------------------------------------- #
 # ------------------------------------------------- PROCESSING DATA ------------------------------------------------- # 
@@ -218,10 +237,13 @@ aws.Spot.interruption.freq.process()
 
 ## FINAL DATA SET
 create.all.prices.dataset()
+create.all.prices.dataset.min()
 
 # create subsets for versions of algorithm
 aws.all.prices.OD <- aws.all.prices[ , 1:5]
 aws.all.prices.OD.RI.SP <- aws.all.prices[ , 1:29]
+
+
 
 
 
@@ -234,6 +256,7 @@ aws.all.prices.OD.RI.SP <- aws.all.prices[ , 1:29]
 #   - Spot prices are averaged over the last 3 months since the retrieval of the data
 #   - Spot interruption frequency intervals are set to the mean value of lower and upper bound
 #   - RI Market prices are not included
+#   - vCPUs are equal to real cores -> OFFEN (adapt intel and amd instances)
 
 
-# write.csv(aws.shared.all.prices, "~/bachelor-thesis/aws.shared.all.prices.csv", row.names=FALSE)
+# write.csv(aws.all.prices, "~/bachelor-thesis/aws.all.prices.csv", row.names=FALSE)
