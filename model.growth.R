4b6a341b6e7c449b3b85a3c9bd637ab8ccfc25ad
marceldietl <ge93coc>
2023-08-03 16:38:28 +0200

    Updated options for growth

--- model.growth.R
+++ model.growth.R
@@ -1,62 +1,96 @@
 source("./model.R")
+source("./snowset.analysis.R")
 
 ## ------------------------------------------------------------------------------------------------------------ ##
                                                 # Model with Growth
 ## ------------------------------------------------------------------------------------------------------------ ##
 
-# Recalculate every time period where the growth rate applies, e.g. yearly
-# checks whether growth is so little that it can be covered by the plan initially bought
-# or whether a new plan needs to be acquired (as this is cheaper than buying a larger instance directly)
-
-algorithm.with.growth <- function(growth.rate, workload.base, workload.fluct) {
-  new.workload.base <- (workload.base * (1 + growth.rate))
-  new.workload.fluct <- lapply(workload.fluct, function(x) x * (1 + growth.rate))
+# Assuming a particular growth rate, calculates whether it is better to keep the base load the same and 
+# cover the additional growth through spot instances or if it is better to increase the volumes of RIs or SPs
+algorithm.with.growth <- function(growth.rate, workload.base, workload.fluct, migration.costs) {
   
-  result.wo.growth <- find.cheapest.instance.final(workload.base, workload.fluct, 2)
-  result.with.growth <- find.cheapest.instance.final(new.workload.base, new.workload.fluct, 2)
+  # adapt base and fluct
+  new.workload.base.o1 <- (workload.base * (1 + growth.rate))
+  new.workload.fluct.o1 <- lapply(workload.fluct, function(x) (x * (1 + growth.rate)-new.workload.base.o1))
+  new.workload.fluct.o1 <- ifelse(new.workload.fluct.o1 < 0, 0, new.workload.fluct.o1)
   
-  if (result.with.growth[1, 'Total_Costs'] == result.wo.growth[1, 'Total_Costs'] && 
-      result.with.growth[1, 'Num_Instances_Required'] == result.wo.growth[1, 'Num_Instances_Required']) {
+  # keep base and adapt fluct
+  new.workload.base.o2 <- workload.base
+  new.workload.fluct.o2 <- lapply(workload.fluct, function(x) (x * (1 + growth.rate)-new.workload.base.o2))
+  new.workload.fluct.o2 <- ifelse(new.workload.fluct.o2 < 0, 0, new.workload.fluct.o2)
+  
+  # calculate results for both options
+  result.with.growth.o1 <<- find.cheapest.instance.final(new.workload.base.o1, new.workload.fluct.o1, migration.costs)
+  result.with.growth.o2 <<- find.cheapest.instance.final(new.workload.base.o2, new.workload.fluct.o2, migration.costs)
+  
+  # check if new used instance in o1 is fully exploited - adapt parallelization fraction as needed
+  vCPU <- aws.all.prices$vCPUs[aws.all.prices$API.Name == result.with.growth.o1[1, "Instance_Type"]]
+  vCPU <- vCPU * floor(amdahl(0.95, as.numeric(result.with.growth.o1[1, "Num_Instances_Required"])))
+  if (new.workload.base.o1 != vCPU) {
+    new.workload.base.o1 <- vCPU
+    new.workload.fluct.o1 <- lapply(workload.fluct, function(x) (x * (1 + growth.rate)-new.workload.base.o1))
+    new.workload.fluct.o1 <- ifelse(new.workload.fluct.o1 < 0, 0, new.workload.fluct.o1)
     
-    message("No new price plan required")
-    return(result.with.growth)
-    
-  } else {
-    new.workload.base <- (workload.base * (1 + growth.rate)) - workload.base
-    result.with.growth <- find.cheapest.instance.final(new.workload.base, new.workload.fluct, 2)
-    message("New price plan required")
-    return(result.with.growth)
+    # recalc result
+    result.with.growth.o1 <<- find.cheapest.instance.final(new.workload.base.o1, new.workload.fluct.o1, migration.costs)
   }
+  
+  # calculate total costs
+  o1.base <- result.with.growth.o1[1, 'Total_Costs'] * 24
+  o1.fluct <- sum(result.with.growth.o1[2:25, 'Total_Costs'])
+  o1.total <<- o1.base + o1.fluct
+  
+  o2.base <- result.with.growth.o2[1, 'Total_Costs'] * 24
+  o2.fluct <- sum(result.with.growth.o2[2:25, 'Total_Costs'])
+  o2.total <<- o2.base + o2.fluct
+  
+  if (o1.total < o2.total) {
+    print("Buy additional committed plans")
+  } else {
+    print("Keep base workload and cover growth with spot instances")
+  }
+  
+  # if (result.with.growth[1, 'Total_Costs'] == result.wo.growth[1, 'Total_Costs'] && 
+  #     result.with.growth[1, 'Num_Instances_Required'] == result.wo.growth[1, 'Num_Instances_Required']) {
+  #   
+  #   message("No new price plan required")
+  #   return(result.with.growth)
+  #   
+  # } else {
+  #   new.workload.base <- (workload.base * (1 + growth.rate)) - workload.base
+  #   result.with.growth <- find.cheapest.instance.final(new.workload.base, new.workload.fluct, 2)
+  #   message("New price plan required")
+  #   return(result.with.growth)
+  # }
 }
 
-df.example.growth <- algorithm.with.growth(0.20, 100, list(16, 24, 0, 17, 18, 24, 3, 8, 24, 8, 15, 26, 17, 18, 50, 3, 8, 14, 10, 21, 15, 18, 6, 2))
 
-df.example.grwoth2 <- algorithm.with.growth(0.20, 100, list(16, 24, 0, 17, 18, 24, 3, 8, 24, 8, 15, 26, 17, 18, 50, 3, 8, 14, 10, 21, 15, 18, 6, 2))
+## ------------------------------------------------------------------------------------------------------------ ##
+                                        # Example usage - Synthetic workload
+## ------------------------------------------------------------------------------------------------------------ ##
 
-sum(df.example.growth[ , "Total_Costs"])
-
-## RESULTS
-
-## Bei 20% Wachstum pro Jahr -- 100 CPUh/h
-# Wenn direkt teurer gekauft wird: c6a.32xlarge für 1.87609$
-# Wenn ursprünglicher Preis + dann extra plan für Wachstum: 11x a1.metal für insg. 1.683$ + 7x a1.xlarge für insg. 0.268$ = 1.951$
-
-## Bei 15% Wachstum pro Jahr -- 100 CPUh/h
-# Wenn direkt teurer gekauft wird: c6a.32xlarge für 1.87609$
-# Wenn ursprünglicher Preis + dann extra plan für Wachstum: 11x a1.metal für insg. 1.683$ + 1x a1.metal für insg. 0.153$ = 1.836$
-
-## Bei 15% Wachstum pro Jahr -- 10 CPUh/h
-# Wenn direkt teurer gekauft wird: a1.metal für 0.153$
-# Wenn ursprünglicher Preis + dann extra plan für Wachstum: 7x a1.large für insg. 0.134$ + 1x a1.large für insg. 0.019$ = 0.153$
-
-## Bei 10% Wachstum pro Jahr  -- 100 CPUh/h
-# 11x a1.metal für insg. 1.683$
-
-## Bei 5% Wachstum pro Jahr -- 100 CPUh/h
-# 11x a1.metal für insg. 1.683$
-
-# -> Je größer das Wachstum, desto teurer die zusätzliche Instanzkonfiguration die man kaufen müsste -> Gleich eine größere lohnt mehr
-
- 
+df.example.growth <- algorithm.with.growth(0.50, 
+                                           8, 
+                                           list(6, 6, 7, 7, 6, 7, 8, 6.5, 7.5, 24, 32, 6.3, 
+                                                6.5, 6, 7, 6.5, 8, 8, 6.3, 6.7, 7.2, 6.5, 7.6, 6),
+                                           2)
 
 
+## ------------------------------------------------------------------------------------------------------------ ##
+                                      # Example usage - Snowset workload (VH-1)
+## ------------------------------------------------------------------------------------------------------------ ##
+
+df.example.growth2 <- algorithm.with.growth(0.20, 
+                                            96, 
+                                            as.list(snowset.large.cust.hourly$totalCpuTime),
+                                            2)
+
+
+## ------------------------------------------------------------------------------------------------------------ ##
+                                      # Example usage - Snowset workload (VH-2)
+## ------------------------------------------------------------------------------------------------------------ ##
+
+df.example.growth3 <- algorithm.with.growth(0.20, 
+                                            8, 
+                                            as.list(snowset.middle.cust.hourly$totalCpuTime),
+                                            1)
